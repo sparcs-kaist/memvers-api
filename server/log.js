00ab5ef2cf8e64f1e0ce0b4d5d1545137ac000b4
Jaemin Hong <hjm0901@gmail.com>
2019-03-03 14:06:52 +0000

    no ip

--- server/log.js
+++ server/log.js
@@ -3,12 +3,11 @@ function logStr(str) {
 }
 
 function log(req, _obj) {
-  let ip = req.ip;
   let url = req.url;
   let _un = req.session.un;
   let un = _un ? _un : '';
   let obj = _obj ? _obj.toString() : '';
-  logStr(`${ip}\t${url}\t${un}\t${obj}`);
+  logStr(`${url}\t${un}\t${obj}`);
 }
 
 function logError(req, err) {
