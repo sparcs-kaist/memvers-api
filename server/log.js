1fe6c5f9a87cf9c5ae04ae43ef20d2ebd5df4354
Jaemin Hong <hjm0901@gmail.com>
2019-03-08 14:20:08 +0000

    fix log

--- server/log.js
+++ server/log.js
@@ -1,21 +1,17 @@
-function logStr(str) {
+function str(str) {
   if (str) console.log(new Date().toString() + '\t' + str);
 }
 
 function log(req, _obj) {
-  let url = req.url;
+  let url = req.originalUrl;
   let _un = req.session.un;
   let un = _un ? _un : '';
   let obj = _obj ? _obj.toString() : '';
   logStr(`${url}\t${un}\t${obj}`);
 }
 
-function logError(req, err) {
+function error(req, err) {
   if (err) log(req, err);
 }
 
-module.exports = {
-  log: log,
-  logError: logError,
-  logStr: logStr,
-};
+module.exports = { error, str };
