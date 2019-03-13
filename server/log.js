23a7bdd24465b3630271db8b99290bc24f1d9565
Jaemin Hong <hjm0901@gmail.com>
2019-03-13 22:05:01 +0000

    log methods

--- server/log.js
+++ server/log.js
@@ -6,11 +6,12 @@ function str(str) {
 }
 
 function req(req, _obj) {
+  let method = req.method;
   let url = req.originalUrl;
   let _un = req.session.un;
   let un = _un ? _un : '';
   let obj = _obj ? _obj.toString() : '';
-  str(`${url}\t${un}\t${obj}`);
+  str(`${method}\t${url}\t${un}\t${obj}`);
 }
 
 function error(err) {
