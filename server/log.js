654331df2631a55bb4c1d509beaca8f1fbe3e63e
Khinenw <deu07115@gmail.com>
2020-09-16 21:55:17 +0900

    fix: Fix plain password is being logged

--- server/log.js
+++ server/log.js
@@ -18,6 +18,23 @@ function error(err) {
   if (err) {
     /* eslint-disable no-console */
     console.log(new Date().toString());
+    if(err && typeof err === 'object') {
+      if (err.command) {
+        // To prevent plain password being logged for failed ldap commands
+        let errorStr = `Error while executing ${err.command}`;
+
+        if (err.stdout) errorStr += `\nstdout: ${err.stdout}`;
+        if (err.stderr) errorStr += `\nstderr: ${err.stderr}`;
+        if (err.err) {
+          err.err.message = errorStr;
+          console.log(err.err);
+          return;
+        }
+
+        console.log(errorStr);
+        return;
+      }
+    }
     console.log(err);
     /* eslint-enable no-console */
   }
