d9af91edbd88d19223d0608aa7629c125d5f6303
Khinenw <deu07115@gmail.com>
2020-09-16 16:58:13 +0900

    feat: Add force changing password

--- server/routers/passwd.js
+++ server/routers/passwd.js
@@ -5,7 +5,6 @@ const { success, errorWith, json } = require('../response.js');
 const { checkPassword } = require('../util.js');
 
 const router = express.Router();
-router.use(auth.loginOnly);
 
 /**
  * @api {post} /passwd Passwd
@@ -24,7 +23,7 @@ router.use(auth.loginOnly);
  *
  * @apiError (Error 401) Unauthorized Not logged in
  */
-router.post('/', (req, res) => {
+router.post('/', auth.loginOnly, (req, res) => {
   let un = req.session.un;
   let opass = req.body.opass;
   let npass = req.body.npass;
@@ -35,6 +34,38 @@ router.post('/', (req, res) => {
     .catch(errorWith(0))
     .then(json(res));
   else res.json(errorWith(1)());
+});
+
+/**
+ * @api {post} /passwd/admin/:un Force Passwd
+ * @apiName Force Passwd
+ * @apiGroup Passwd
+ * @apiDescription Change a password by admin permission
+ *
+ * @apiParam {String} npass A new password
+ *
+ * @apiSuccess {Boolean} success Indicate whether succeeded
+ * @apiSuccess {Number} error The reason of the failure (
+ * <code>undefined</code> if succeeded;
+ * <code>0</code> if <code>un</code> does not exist;
+ * <code>1</code> if <code>npass</code> is weak)
+ *
+ * @apiError (Error 401) Unauthorized Not logged in
+ * @apiError (Error 403) Forbidden Not a wheel account
+ */
+router.post('/admin/:un', auth.wheelOnly, (req, res) => {
+  const un = req.params.un;
+  const npass = req.body.npass;
+
+  if (!checkPassword(npass, un)) {
+    res.json(errorWith(1)());
+    return;
+  }
+
+  ldap.passwdByAdmin(un, npass)
+    .then(success)
+    .catch(errorWith(0))
+    .then(json(res))
 });
 
 module.exports = router;
